f1ae938d14148b4e68ae5784e6dc06783d900317
Anthony Angus <anthony.angus@intel.com>
2019-08-13 13:31:37 -0700

    now does rename fine and looks for size of file, but still not changing correct lines... yet

--- crawl_ff.py
+++ crawl_ff.py
@@ -1,4 +1,4 @@
-import glob, os, shutil
+import glob, os, shutil, mmap
 
 print("this is so exciting... let's find files and folders! :)")
 # os.chdir("/test_folder")
@@ -28,7 +28,7 @@ print("this is so exciting... let's find files and folders! :)")
 
 def lowercase_rename( dir ):
     # renames all subforders of dir, not including dir itself
-    def rename_all( root, items):
+    def rename_all( root, items ):
         for name in items:
             try:
                 os.rename( os.path.join(root, name), 
@@ -41,4 +41,43 @@ def lowercase_rename( dir ):
         rename_all( root, dirs )
         rename_all( root, files)
 
+def lowercase_includes( dir ):
+    def rename_includes( root, items ):
+        for name in items:
+            try:
+                # with open(items, 'rb', 0) as file, mmap.mmap(file.fileno(), 0, access.ACCESS_READ) as s:
+                #     if s.find("#include \""):
+                #         s = s.lower()
+                # print("checking file: " + name)
+                # if os.path.getsize(name) > 0:
+                print ("file: " + name + ", size: " \
+                        + str(os.path.getsize(os.path.abspath(os.path.join(root, name)))))
+                        # + os.path.getsize(os.path.abspath(name)))
+                        #+ os.stat(os.path.abspath(name)).st_size > 0)
+                # print (os.stat(os.path.abspath(name)).st_size)
+                
+                # print (str(os.path.getsize(os.path.abspath(name))))
+                
+
+                if os.path.getsize(os.path.abspath(os.path.join(root, name))) > 0: #os.path.getsize(name) > 0:
+                    # print("checking file: " + name)
+                    # print (str(os.path.getsize(os.path.abspath(os.path.join(root, name)))))
+                    with open( os.path.abspath(os.path.join(root, name)), 'w') as fileinput:
+                        for line in fileinput:
+                            print("found upper case on: " + line)
+                            # if line[:9] == "#include \"":
+                            if "#include" in line:
+                                line = line.lower()
+                                print("new line: " + line)
+                                fileinput.write(line)
+            # except OSError:
+            except Exception as err:
+                print("An exception happened: " + str(err))
+                # print("error")
+                pass
+    
+    for root, dirs, files in os.walk( dir, topdown=False ):
+        rename_includes( root, files )
+
 lowercase_rename("tfc")
+lowercase_includes("tfc")
