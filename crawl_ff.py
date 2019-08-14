f01c14e82227fd457dcc04e38a16a46a25b694d8
Anthony Angus <anthony.angus@intel.com>
2019-08-14 14:14:53 -0700

    added #include test case in root and coped elsewhere just to confirm & fixed argument parser

--- crawl_ff.py
+++ crawl_ff.py
@@ -1,30 +1,4 @@
-import os, fileinput, argparse
-
-print("this is so exciting... let's find files and folders! :)")
-# os.chdir("/test_folder")
-
-# def crawl(start_dir):    
-#     # for root, dirs, files in os.walk(start_dir):
-#     for dir in dirs:
-#         print("found directory:" + dir + " renamed to: " dir.lower())
-#         os.rename(os.path.join(os.getcwd(), dir), os.path.join(os.getcwd(), dir.lower())
-#         crawl(dir.lower())
-#     for file in files:
-#         print("found file: " + file)
-#         # if file.endswith(".txt"):
-#         #      print(os.path.join(root, file))
-#         # os.rename(file, file.replace(' ', '_').lower())
-#         # os.rename(file, file.lower() + "temp")
-#         # os.rename(file, file.lower())
-#         # shutil.move(file, file.lower() + "temp")
-#         # shutil.move(file, file.lower() - "temp")
-
-#         # root, ext = os.path.splitext(file)
-#         # print("Renaming", file, "to", root.lower())
-#         # os.rename(os.path.join(os.getcwd(), file), os.path.join(os.getcwd(), file.lower()))
-
-# crawl("tfc")
-
+import os, sys, fileinput, argparse
 
 def lowercase_rename( dir ):
     # renames all subforders of dir, not including dir itself
@@ -56,16 +30,33 @@ def lowercase_includes( dir ):
     for root, dirs, files in os.walk( dir, topdown=False ):
         rename_includes( root, files )
 
+def valid_dir(name):
+    if os.path.isdir(name):
+        return name
+    else:
+        raise argparse.ArgumentTypeError(f"{name} is not a valid directory")
+
+
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', dest='dir',
-                        type=str, help='the root directory in which to modify \
+                        type=valid_dir, help='the root directory in which to modify \
                         lowercase files/directories and found #includes')
     args = parser.parse_args()
 
+    print("This is so exciting...")
     if args.dir is None:
         print("need to specify directory with -d or --directory")
         sys.exit(0)
-    else:    
+    else:
+        dir = args.dir #os.path.abspath(args.dir)
+        print(f"Let's find files and folders in {dir}!")
         lowercase_rename(dir)
+        print("Now for the found #includes!")
         lowercase_includes(dir)
+        print("... done 'n done :)")
+
+
+# call main if this script is being called by itself
+if __name__ == '__main__':
+    _main()
